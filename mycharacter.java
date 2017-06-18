b1a60759b7c26641d0f7f6686b411c9b2527c736
exactl156 <rohan3637@gmail.com>
2017-06-18 11:19:59 -0700

    Add files via upload

--- mycharacter.java
+++ mycharacter.java
@@ -1,18 +1,64 @@
 package chess;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public abstract class mycharacter extends JButton {
 protected int Xpos;
 protected int Ypos;
-protected 
 
-	public void move() {
-		// TODO Auto-generated method stub
+protected boolean isSelected;
+private ActionListener l;
+
+
+	public mycharacter(int xpos, int ypos) {
+	super();
+	Xpos = xpos;
+	Ypos = ypos;
+	isSelected = false;	
+	l = new ActionListener() {
 		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(!isSelected)
+			{
+				isSelected=true;
+			}
+		}
+	};
+	this.addActionListener(l);
 	}
 
-	public mycharacter getUserInput(mycharacter y) {
+	public void move()
+	{
+		
+	};
+
+	public int getXpos() 
+	{
+	return Xpos;
+	}
+
+public void setXpos(int xpos) 
+{
+	Xpos = xpos;
+}
+
+public int getYpos() 
+{
+	return Ypos;
+}
+
+public void setYpos(int ypos) 
+{
+	Ypos = ypos;
+}
+
+	public mycharacter getUserInput(mycharacter y) 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
