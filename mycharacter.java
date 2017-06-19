11a5b33d01cfc97d58af0c38c839e7ed1d78ee57
exactl156 <rohan3637@gmail.com>
2017-06-18 23:27:36 -0700

    Add files via upload

--- mycharacter.java
+++ mycharacter.java
@@ -1,18 +1,71 @@
 package chess;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
-public abstract class mycharacter extends JButton {
+public class mycharacter extends JButton {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1077474492673682496L;
 protected int Xpos;
 protected int Ypos;
-protected 
 
-	public void move() {
-		// TODO Auto-generated method stub
+public boolean isSelected;
+private ActionListener l;
+
+
+	public mycharacter(int xpos, int ypos) {
+	super();
+	Xpos = xpos;
+	Ypos = ypos;
+	if(Xpos%2==1)
+	this.setBackground(Color.BLACK);
+	isSelected = false;	
+	l = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			if(!isSelected)
+			{
+				isSelected=true;
+			}
+		}
+	};
+	this.addActionListener(l);
+	}
+
+	public void move()
+	{
 		
 	}
 
-	public mycharacter getUserInput(mycharacter y) {
+	public int getXpos() 
+	{
+	return Xpos;
+	}
+
+public void setXpos(int xpos) 
+{
+	Xpos = xpos;
+}
+
+public int getYpos() 
+{
+	return Ypos;
+}
+
+public void setYpos(int ypos) 
+{
+	Ypos = ypos;
+}
+
+	public mycharacter getUserInput(mycharacter y) 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
